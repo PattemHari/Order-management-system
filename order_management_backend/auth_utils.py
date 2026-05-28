47899f302ec068596d0aca1ea26f41a826a52577
PattemHari <sreehari.pattem@gmail.com>
2026-05-28 16:11:32 +0530

    Changes

--- order_management_backend/auth_utils.py
+++ order_management_backend/auth_utils.py
@@ -12,6 +12,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # password helpers
 def hash_password(password: str) -> str:
+    #First branch chnages
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
